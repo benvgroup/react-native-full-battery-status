2c695523d4b9faef2186df242293821e2c20d337
Lau Borges <lauborgesc@gmail.com>
2018-12-25 14:43:22 -0200

    Main interfaces

--- android/src/main/java/com/benvgroup/BatteryStatus.java
+++ android/src/main/java/com/benvgroup/BatteryStatus.java
@@ -4,45 +4,181 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.BatteryManager;
+import android.os.Build;
 
 public class BatteryStatus extends BroadcastReceiver {
 
-  private int plugged;
-  private String batteryStatus;
+  private String healthStatus;
+  private String pluggedStatus = "NONE";
+  private String chargingStatus;
+  private String technology;
+  private int batteryPercent;
+  private int temperature;
+  private int voltage;
+  private float temperatureResult;
+  private long capacity;
+  private Context context;
 
   @Override
   public void onReceive(Context context, Intent intent) {
-    boolean isPresent = intent.getBooleanExtra("present", false);
-    plugged = intent.getIntExtra("plugged", -1);
+    this.context = context;
+    updateBatteryData(intent);
+  }
 
-    if (isPresent) {
-      setBatteryStatus("Battery present");
-    } else {
-      setBatteryStatus("Battery not present!");
+  private void updateBatteryData(Intent intent) {
+    boolean present = intent.getBooleanExtra(BatteryManager.EXTRA_PRESENT, false);
+
+    if (present) {
+      int health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, 0);
+
+      switch (health) {
+        case BatteryManager.BATTERY_HEALTH_COLD:
+          healthStatus = "COLD";
+          break;
+
+        case BatteryManager.BATTERY_HEALTH_DEAD:
+          healthStatus = "DEAD";
+          break;
+
+        case BatteryManager.BATTERY_HEALTH_GOOD:
+          healthStatus = "GOOD";
+          break;
+
+        case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+          healthStatus = "OVER_VOLTAGE";
+          break;
+
+        case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+          healthStatus = "OVER_HEAT";
+          break;
+
+        case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+          healthStatus = "UNSPECIFIED_FAILURE";
+          break;
+
+        case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+        default:
+          break;
+      }
+
+      int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+      int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+      if (level != -1 && scale != -1) {
+        batteryPercent = (int) ((level / (float) scale) * 100f);
+      }
+
+      int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0);
+
+      switch (plugged) {
+        case BatteryManager.BATTERY_PLUGGED_WIRELESS:
+          pluggedStatus = "WIRELESS";
+          break;
+
+        case BatteryManager.BATTERY_PLUGGED_USB:
+          pluggedStatus = "USB";
+          break;
+
+        case BatteryManager.BATTERY_PLUGGED_AC:
+          pluggedStatus = "AC";
+          break;
+
+        default:
+          pluggedStatus = "NONE";
+          break;
+      }
+
+      int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+
+      switch (status) {
+        case BatteryManager.BATTERY_STATUS_CHARGING:
+          chargingStatus = "CHARGING";
+          break;
+
+        case BatteryManager.BATTERY_STATUS_DISCHARGING:
+          chargingStatus = "DISCHARGING";
+          break;
+
+        case BatteryManager.BATTERY_STATUS_FULL:
+          chargingStatus = "FULL";
+          break;
+
+        case BatteryManager.BATTERY_STATUS_UNKNOWN:
+          chargingStatus = "UNKNOWN";
+          break;
+
+        case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+        default:
+          chargingStatus = "NOT_CHARGING";
+          break;
+      }
+
+      if (intent.getExtras() != null) {
+        technology = intent.getExtras().getString(BatteryManager.EXTRA_TECHNOLOGY);
+      }
+
+      temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0);
+
+      if (temperature > 0) {
+        temperatureResult = ((float) temperature) / 10f;
+      }
+
+      voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0);
+
+      capacity = getBatteryCapacity(this.context);
     }
+
   }
 
-  public String getPlugTypeString() {
-    String plugType = "NONE";
+  public long getBatteryCapacity(Context ctx) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      BatteryManager mBatteryManager = (BatteryManager) ctx.getSystemService(Context.BATTERY_SERVICE);
+      Long chargeCounter = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER);
+      Long capacity = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
 
-    switch (plugged) {
-      case BatteryManager.BATTERY_PLUGGED_AC:
-        plugType = "AC";
-        break;
-      case BatteryManager.BATTERY_PLUGGED_USB:
-        plugType = "USB";
-        break;
+      if (chargeCounter != null && capacity != null) {
+        long value = (long) (((float) chargeCounter / (float) capacity) * 100f);
+        return value;
+      }
     }
 
-    return plugType;
+    return 0;
   }
 
-  private void setBatteryStatus(String text) {
-    batteryStatus = text;
+  public String getHealthStatus() {
+    return healthStatus;
   }
 
-  private String getBatteryStatus() {
-    return batteryStatus;
+  public String getPluggedStatus() {
+    return pluggedStatus;
+  }
+
+  public String getChargingStatus() {
+    return chargingStatus;
+  }
+
+  public String getTechnology() {
+    return technology;
+  }
+
+  public int getBatteryPercent() {
+    return batteryPercent;
+  }
+
+  public int getTemperature() {
+    return temperature;
+  }
+
+  public int getVoltage() {
+    return voltage;
+  }
+
+  public float getTemperatureResult() {
+    return temperatureResult;
+  }
+
+  public long getCapacity() {
+    return capacity;
   }
 
 }
--- android/src/main/java/com/benvgroup/RNFullBatteryStatusModule.java
+++ android/src/main/java/com/benvgroup/RNFullBatteryStatusModule.java
@@ -46,9 +46,79 @@ public class RNFullBatteryStatusModule extends ReactContextBaseJavaModule {
    *  pluggedType() Returns the plugged type. USB, AC, NONE
    */
   @ReactMethod
-  public void pluggedType(final Promise promise) {
+  public void getPluggedStatus(final Promise promise) {
     try {
-      String type = batteryStatus.getPlugTypeString();
+      String type = batteryStatus.getPluggedStatus();
+      promise.resolve(type);
+    } catch (Exception ex) {
+      promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+    }
+  }
+
+  @ReactMethod
+  public void getHealthStatus(final Promise promise) {
+    try {
+      String type = batteryStatus.getHealthStatus();
+      promise.resolve(type);
+    } catch (Exception ex) {
+      promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+    }
+  }
+
+  @ReactMethod
+  public void getChargingStatus(final Promise promise) {
+    try {
+      String type = batteryStatus.getChargingStatus();
+      promise.resolve(type);
+    } catch (Exception ex) {
+      promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+    }
+  }
+
+  @ReactMethod
+  public void getTechnology(final Promise promise) {
+    try {
+      String type = batteryStatus.getTechnology();
+      promise.resolve(type);
+    } catch (Exception ex) {
+      promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+    }
+  }
+
+  @ReactMethod
+  public void getBatteryPercent(final Promise promise) {
+    try {
+      int type = batteryStatus.getBatteryPercent();
+      promise.resolve(type);
+    } catch (Exception ex) {
+      promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+    }
+  }
+
+  @ReactMethod
+  public void getTemperature(final Promise promise) {
+    try {
+      float type = batteryStatus.getTemperatureResult();
+      promise.resolve(type);
+    } catch (Exception ex) {
+      promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+    }
+  }
+
+  @ReactMethod
+  public void getVoltage(final Promise promise) {
+    try {
+      int type = batteryStatus.getVoltage();
+      promise.resolve(type);
+    } catch (Exception ex) {
+      promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+    }
+  }
+
+  @ReactMethod
+  public void getCapacity(final Promise promise) {
+    try {
+      long type = batteryStatus.getCapacity();
       promise.resolve(type);
     } catch (Exception ex) {
       promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
